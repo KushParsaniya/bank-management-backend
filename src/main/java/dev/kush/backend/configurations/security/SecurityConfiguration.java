9092a2e1c74d35d5f162e227d42da8956ac74d36
kush parsaniya <102735354+KushParsaniya@users.noreply.github.com>
2024-02-26 21:34:27 +0530

    Update SecurityConfiguration.java

--- src/main/java/dev/kush/backend/configurations/security/SecurityConfiguration.java
+++ src/main/java/dev/kush/backend/configurations/security/SecurityConfiguration.java
@@ -80,7 +80,7 @@ public class SecurityConfiguration {
         cors.setAllowedOrigins(List.of("https://easybankdev.vercel.app","http://localhost:3000"));
         cors.setMaxAge(3600L);
         cors.setAllowedMethods(List.of("*"));
-        cors.setExposedHeaders(List.of("Authorization"));
+        cors.setExposedHeaders(List.of("*"));
         cors.setAllowCredentials(true);
 
         return cors;
